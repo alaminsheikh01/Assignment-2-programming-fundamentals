b4963033ee1ff8e4c820008683994a1bd4c8acf1
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-02-07 23:16:59 +0600

    pass parameter

--- array.js
+++ array.js
@@ -1,9 +1,17 @@
 const numbers = [2,3,4,5,6,7]
 
-let sum = 0;
-for(let i = 0; i < numbers.length; i++){
-    console.log(numbers[i] * 2)
-    sum += numbers[i]
+// let sum = 0;
+// for(let i = 0; i < numbers.length; i++){
+//     console.log(numbers[i] * 2)
+//     sum += numbers[i]
 
+// }
+// console.log(sum)
+
+// callback function
+
+const cb = (value, index, arr) =>{
+    console.log(value, index, arr)
 }
-console.log(sum)
+
+numbers.forEach(cb)
