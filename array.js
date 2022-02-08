541f09859ffd7bb34c9559b1bfaf0baa4f8b5c1e
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-02-08 20:57:04 +0600

    slice array by using impretative way

--- array.js
+++ array.js
@@ -10,8 +10,32 @@ const numbers = [2,3,4,5,6,7]
 
 // callback function
 
-const cb = (value, index, arr) =>{
-    console.log(value, index, arr)
+// const cb = (value, index, arr) =>{
+//     console.log(value, index, arr)
+// }
+
+// numbers.forEach(cb)
+
+
+// imperative way
+
+const arr = [1,2,3, null, false, 4,5,6, '', 'test', 9]
+
+let count = 0;
+
+for(let i=0; i < arr.length; i++)
+{
+    for(let j=i; j < arr.length - 1; j++){
+        if(!arr[j] || typeof arr[j] !== 'number'){
+            arr[j] = arr[j+1]
+            arr[j+1] = undefined;
+        }
+    }
+
+    if(arr[i] == undefined){
+        count++;
+    }
 }
 
-numbers.forEach(cb)
+arr.length -= count;
+console.log(arr)
