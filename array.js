b74d84d9827e83066d35f42bd2d7be2ce4623dd8
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-02-10 21:47:55 +0600

    load url data with reduce

--- array.js
+++ array.js
@@ -211,13 +211,18 @@ const url = 'https://jsonplaceholder.typicode.com/posts'
 
 async function getData() {
     const {data} = await axios.get(url);
-    const result = data.map(item => {
-        return {
-            userId: item.userId,
-            id: item.id,
-            title: item.title,
-        }
-    })
+    // const result = data.splice(0,10).map(item => {
+    //     return {
+    //         userId: item.userId,
+    //         id: item.id,
+    //         title: item.title,
+    //     }
+    // })
+    const result = data.splice(0,10).reduce((acc, cur) =>{
+        acc[cur.id] = {...cur};
+        delete acc[cur.id].body;
+        return acc;
+    },{})
     return result;
 }
 
